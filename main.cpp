45fbee59eb85fca552f7f6d45d74cdc72cf31c3c
wanderer <dutifulwanderer@gmail.com>
2024-10-11 22:02:33 +0800

    read excel

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ extern "C"
     #include "lua.h"    
 }
 
+#include <iostream>
 #include <xlnt/xlnt.hpp>
 
 int main(int argc,char* args[])
@@ -13,12 +14,26 @@ int main(int argc,char* args[])
     luaL_openlibs(L);
     lua_close(L);
     
-    xlnt::workbook wb;
-    xlnt::worksheet ws = wb.active_sheet();
+    // xlnt::workbook wb;
+    // xlnt::worksheet ws = wb.active_sheet();
 
-    ws.cell("A1").value(5);
-    ws.cell("B2").value("string data");
-    wb.save("example.xlsx");
+    // ws.cell("A1").value(5);
+    // ws.cell("B2").value("string data");
+    // wb.save("example.xlsx");
+
+    xlnt::workbook wb;
+    wb.load("example.xlsx");
+
+    auto ws = wb.active_sheet();
+    for (auto row : ws.rows(true))
+    {
+        for (auto cell : row)
+        {
+            std::cout<< cell.to_string()<<std::endl;
+        }
+        
+    }
+    
 
     return 0;
 }
